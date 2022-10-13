64bf28c41f3eaeb3bf12cb325a99e210a8014393
georgec7 <49031532+georgec7@users.noreply.github.com>
2022-10-13 13:34:48 -0400

    Create Project
    Create Project

---
+++ Project1/Project1/main.asm
@@ -0,0 +1,16 @@
+;-----------------------------------------------------------------------------
+; Assembly main line
+;-----------------------------------------------------------------------------
+
+include "m8c.inc"       ; part specific constants and macros
+include "memory.inc"    ; Constants & macros for SMM/LMM and Compiler
+include "PSoCAPI.inc"   ; PSoC API definitions for all User Modules
+
+export _main
+
+_main:
+
+    ; Insert your main assembly code here.
+
+.terminate:
+    jmp .terminate
